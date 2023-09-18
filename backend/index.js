5f6b0af04bfcd2a504c93e1440a8fcb28f3aceaa
Mauricio <mauriciornn@gmail.com>
2023-09-18 11:27:14 -0300

    crud quase final 18/09

--- backend/index.js
+++ backend/index.js
@@ -44,11 +44,22 @@ app.get("/pessoa/:id", async (req, res) => {
   return res.status(200).json(query);
 });
 
-app.get("/pessoa/:nome", async (req, res) => {
+app.get("/pessoa/buscarnome/:nome", async (req, res) => {
   const { nome } = req.params;
   const [query] = await connection.execute(
-    "select * from pessoa where nome like '%?%'",
-    [nome]
+    "select * from pessoa where nome like ?",
+    ["%" + nome + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma pessoa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/pessoa/buscaremail/:email", async (req, res) => {
+  const { email } = req.params;
+  const [query] = await connection.execute(
+    "select * from pessoa where email like ?",
+    ["%" + email + "%"]
   );
   if (query.length === 0)
     return res.status(400).json({ mensagem: "Nenhuma pessoa encontrada" });
@@ -57,9 +68,28 @@ app.get("/pessoa/:nome", async (req, res) => {
 
 app.post("/pessoa", async (req, res) => {
   const { nome, email } = req.body;
-  const [query] = await connection.execute(
-    "insert into pessoa (nome, email) values (?,?",
+  const [query] = await conection.execute(
+    "insert into pessoa (nome, email) values (?, ?)",
     [nome, email]
   );
-  return res.json(req.body);
+  return res.json(query);
+});
+
+app.put("/pessoa/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, email } = req.body;
+
+  const [query] = await conection.execute(
+    "update pessoa set nome = ?, email = ? where id = ?",
+    [nome, email, id]
+  );
+  return res.json(query);
+});
+
+app.delete("/pessoa/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await conection.execute("delete from pessoa where id = ?", [
+    id,
+  ]);
+  return res.json(query);
 });
